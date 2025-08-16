0bccb31a227dd7e470c12fe5f5472638e75aa211
Ricardo <ricantony66@gmail.com>
2025-08-16 17:27:47 -0300

    criar referencias JS para elementos do dashboard

---
+++ script.js
@@ -0,0 +1,4 @@
+//pegando os elementos do html para usar no js
+const inputMeta = document.getElementById('nova-meta');
+const btnAdicionarMeta = document.getElementById('adicionar-meta');
+const listaMetas = document.getElementById('metas');
