f2b845b3d1d1f1278eb5cb96a36a2ac714453d48
Ricardo <ricantony66@gmail.com>
2025-08-17 09:43:31 -0300

    implementar função para add metas com checknox

--- script.js
+++ script.js
@@ -2,3 +2,36 @@
 const inputMeta = document.getElementById('nova-meta');
 const btnAdicionarMeta = document.getElementById('adicionar-meta');
 const listaMetas = document.getElementById('metas');
+
+//adicionando o evento de click no botao
+function adicionarMeta() {
+    const metaTexto = inputMeta.value.trim(); //pega o que foi digitado 
+    if (metaTexto === '') { //verifica se o campo esta vazio
+        alert('Por favor, digite uma meta.');
+        return;
+    }
+
+    // cria um novo elemento de lista
+    const li = document.createElement('li');
+
+    // cria um checkbox para marcar a meta como concluída
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    //cria o texto da meta
+    const span = document.createElement('span');
+    span.textContent = metaTexto;
+
+    //quando o checkbox for marcado, a meta sera riscada
+    checkbox.addEventListener('change', function() {
+        if (checkbox.checked) {
+            span.style.textDecoration = 'line-through';//risca o texto
+            span.style.color = 'gray'; //muda a cor do texto riscado
+        } else {
+            span.style.textDecoration = 'none'; //remove o risco do texto
+            span.style.color = 'black'; //volta a cor original do texto
+        }
+    });    
+
+    //botão para remover a meta
+    const btnRemover = document.createElement('button');
